29cbc6d1ccbc45c4dcec52e2052179fc5d78046c
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-22 17:59:01 +0100

    Fine esercizio dadi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -3,13 +3,21 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // Generare numero random per il giocatore 
-var numeroGiocatore = Math.floor(Math.random() * 6 ) * 1;
-console.log(numeroGiocatore)
-// Generare numero random per il computer
-var numeroComputer = Math.floor(Math.random() * 6 ) * 1;
-console.log('il numero del computer è:', numeroComputer)
-// Operazioni logiche
+var numeroGiocatore = Math.floor(Math.random() * 6 ) + 1;
+console.log('il numero del giocatore è: ', numeroGiocatore);
 
-// Stampare nome vincitore
+// Generare numero random per il computer
+var numeroComputer = Math.floor(Math.random() * 6 ) + 1;
+console.log('il numero del computer è:', numeroComputer);
+
+// Operazioni logiche
+if (numeroGiocatore > numeroComputer) {
+    alert('Hai vinto!');
+} else if (numeroComputer > numeroGiocatore) {
+    alert('Hai Perso!');
+} else {
+    alert('Pari!')
+}
+
 
 
