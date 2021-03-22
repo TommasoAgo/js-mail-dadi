b85a74112fb3f5f034db0e9017e7607ce13fb64a
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-03-22 17:48:14 +0100

    Numeri random

---
+++ Dadi/js/script.js
@@ -0,0 +1,15 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. 
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+// Generare numero random per il giocatore 
+var numeroGiocatore = Math.floor(Math.random() * 6 ) * 1;
+console.log(numeroGiocatore)
+// Generare numero random per il computer
+var numeroComputer = Math.floor(Math.random() * 6 ) * 1;
+console.log('il numero del computer è:', numeroComputer)
+// Operazioni logiche
+
+// Stampare nome vincitore
+
+
